a61344bafafdd81a3a988c930f80e3207c3ef57a
alekniko <aleksejs.nikolajevs@gmail.com>
2019-11-01 12:10:27 +0200

    Add strings example

---
+++ 04strings.py
@@ -0,0 +1,9 @@
+# Strings: http://localhost:8889/notebooks/Documents/Complete-Python-3-Bootcamp-master/Complete-Python-3-Bootcamp-master/00-Python%20Object%20and%20Data%20Structure%20Basics/02-Strings.ipynb
+
+print('Hello World 1')
+print('Hello World 2')
+print('Use \\n to print a new line')
+print('\n')
+print('See what I mean?')
+print('Use \\t to print TAB')
+print('\tThis row starts with a tab')
